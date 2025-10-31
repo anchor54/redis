62a44d28fd84e76e047f628b37cffc0ab48753de
anchor54 <ankur.mazumder54@gmail.com>
2025-11-01 02:09:30 +0530

    added code for listening at a given port on localhost

--- app/main.go
+++ app/main.go
@@ -6,24 +6,30 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment the code below to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	host := "0.0.0.0"
+	port := 6379
+	local_address := fmt.Sprintf("%s:%d", host, port)
+	
+	// preparing a listener at port 6379 on localhost
+	l, err := net.Listen("tcp", local_address)
+	if err != nil {
+		fmt.Printf("An Error occured when Listening to localhost at port 6379: %s", err.Error())
+		// exit on error
+		os.Exit(1)
+	}
+	
+	// close the listener before function ends
+	defer l.Close()
+
+	// accept connection
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Printf("An Error occured when accepting connection: %s", err.Error())
+		// exit on error
+		os.Exit(1)
+	}
 }
